a54f892d79e01b9bd72c54bd8e094a2b2ad6c63e
Berlian <berlink.k2@gmail.com>
2019-07-25 03:12:35 +0100

    Fixed tree maker and evaluator

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,16 @@ public class Main {
 		TreeMaker tm = new TreeMaker();
 		TreeEvaluator te = new TreeEvaluator();
 		Token name = new Token(TokenType.ID, "a", null, 1);
-		Expr expr = new Expr.Assign(name, new Expr.Literal(45));
-		Stmt stmt = new Stmt.Expression(expr);
+		Token name2 = new Token(TokenType.ID, "b", null, 1);
+		Token op = new Token(TokenType.PLUS, "+", null, 1);
+		Expr expr = new Expr.Assign(name, new Expr.Literal(5));
+		Expr expr2 = new Expr.Assign(name2, new Expr.Literal(6));
+		Expr expr3 = new Expr.binOp(op, expr, expr2);
+		Stmt stmt = new Stmt.Expression(expr3);
+		Object result = te.evaluate(expr3);
 		String tree = tm.make(stmt);
-		System.out.println(tree);
+		
+		System.out.println(result);
 		
 		
 		
