2a5ca1acc97491f5ed9e9fc9fea9a6c85b9ebc42
Berlian <berlink.k2@gmail.com>
2019-08-07 19:15:21 +0100

    Adapted Parser.

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,16 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ast.*;
-import ast.Stmt.Expression;
-import ast.Stmt.If;
 import ast.Stmt.Print;
 import ast.Stmt.Var;
 import lexer.*;
+import parser.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-//		Expr left = new Expr.unaryOp(new Token(TokenType.MINUS, "-", null, 1),new Expr.Literal(50.0));
-//		Expr right = new Expr.Literal(66);
-//		Token op = new Token(TokenType.PLUS, "+", null, 1);
-//		Expr tree = new Expr.binOp(op, left, right);
-		
-//		
-//		String exampleTree = tm.make(tree);
-//		Object result = te.evaluate(tree);
-//		
-//		
-//		
-//		System.out.println(exampleTree);
-//		TreeMaker tm = new TreeMaker();
-//		TreeEvaluator te = new TreeEvaluator();
-//		Token name = new Token(TokenType.ID, "a", null, 1);
-//		Token name2 = new Token(TokenType.ID, "b", null, 1);
-//		Token op = new Token(TokenType.PLUS, "+", null, 1);
-//		Expr expr = new Expr.Assign(name, new Expr.Literal(5));
-//		Expr expr2 = new Expr.Assign(name2, new Expr.Literal(6));
-//		Expr expr3 = new Expr.binOp(op, expr, expr2);
-//		
-//		Stmt stmt = new Stmt.Expression(expr3);
-//		Object result = te.evaluate(expr3);
-//		String tree = tm.make(stmt);
-//		
-//		System.out.println(result);
 		
 //		TreeEvaluator eval = new TreeEvaluator();
 //
@@ -50,27 +26,41 @@ public class Main {
 //	    Z = x* (y*10)
 //
 //	   Print z
-		Token token1 = new Token(TokenType.ID, "x", null, 1);
-		Token token2 = new Token(TokenType.ID, "y", null, 1);
-		Token token3 = new Token(TokenType.ID, "z", null, 1);
-		Token mul = new Token(TokenType.STAR, "*", null, 1);
-		
-		// x=2
-		Var x = new Stmt.Var(token1, new Expr.Literal(2));
-		//y=3
-		Var y = new Stmt.Var(token2, new Expr.Literal(3));
-		
-		//x*(y*10)
-		Expr binop1 = new Expr.binOp(mul, x.getInitial(), new Expr.binOp(mul, y.getInitial(), new Expr.Literal(10)));
-		//z=x*(y*10)
-		Var z = new Stmt.Var(token3, binop1);
-		Print print = new Print(z.getInitial());
-		TreeInterpreter eval = new TreeInterpreter();
+//		Token token1 = new Token(TokenType.ID, "x", null, 1);
+//		Token token2 = new Token(TokenType.ID, "y", null, 1);
+//		Token token3 = new Token(TokenType.ID, "z", null, 1);
+//		Token mul = new Token(TokenType.STAR, "*", null, 1);
+//		
+//		// x=2
+//		Var x = new Stmt.Var(token1, new Expr.Literal(2));
+//		//y=3
+//		Var y = new Stmt.Var(token2, new Expr.Literal(3));
+//		
+//		//x*(y*10)
+//		Expr binop1 = new Expr.binOp(mul, x.getInitial(), new Expr.binOp(mul, y.getInitial(), new Expr.Literal(10)));
+//		//z=x*(y*10)
+//		Var z = new Stmt.Var(token3, binop1);
+//		Print print = new Print(z.getInitial());
+//		TreeInterpreter eval = new TreeInterpreter();
+//		List<Stmt> stmts = new ArrayList<>();
+//		stmts.add(print);
 		
 		//print z
-		eval.evaluate(print);
+		//eval.evaluate(stmts);
 		
+		String test = "x=2;y=3;z=x*y;print z;";
 		
+		Lexer lexer = new Lexer(test);
+		List<Token> tokenList = lexer.tokenStream();
+		Parser parser = new Parser(tokenList);
+		parser.parse();
+		List<Stmt> stmtList = parser.getStatementList();
+		TreeInterpreter interpreter = new TreeInterpreter();
+		interpreter.evaluate(stmtList);
+	
+//		for (Token token:tokenList) {
+//			System.out.println(token);
+//		}
 		
 		
 	 
