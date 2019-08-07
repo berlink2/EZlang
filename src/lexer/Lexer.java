20b6f3373ac18362482ce408fdd1c27f7c578ad7
Berlian <berlink.k2@gmail.com>
2019-08-07 18:21:28 +0100

    Adapted Lexer.

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -12,31 +12,32 @@ public class Lexer {
 	private int start = 0;
 	private int curr = 0;
 	private int line = 1;
-	static {
-		resKeywords.put("and",    TokenType.AND);                                            
-		resKeywords.put("else",   TokenType.ELSE);                      
-		resKeywords.put("false",  TokenType.FALSE);                                                                 
-		resKeywords.put("if",     TokenType.IF);                        
-		resKeywords.put("null",    TokenType.NULL);                       
-		resKeywords.put("or",     TokenType.OR);                        
-		resKeywords.put("print",  TokenType.PRINT);                                                                
-		resKeywords.put("true",   TokenType.TRUE);                                             
-		resKeywords.put("while",  TokenType.WHILE); 
-	}
+	private boolean isEnd;
+	
+		
+	
 
-	Lexer(String source) {
+	public Lexer(String source) {
 		this.source = source;
+		resKeywords.put("and", TokenType.AND);
+		resKeywords.put("else", TokenType.ELSE);
+		resKeywords.put("false", TokenType.FALSE);
+		resKeywords.put("if", TokenType.IF);
+		resKeywords.put("null", TokenType.NULL);
+		resKeywords.put("or", TokenType.OR);
+		resKeywords.put("print", TokenType.PRINT);
+		resKeywords.put("true", TokenType.TRUE);
+		resKeywords.put("while", TokenType.WHILE);
 	}
 
 	/*
-	 * Method that is fed source code until end of file is
-	 * reached, calls methods that make tokens, and returns list of tokens
-	 * to be parsed.
+	 * Method that is fed source code until end of file is reached, calls methods
+	 * that make tokens, and returns list of tokens to be parsed.
 	 * 
 	 * @return
 	 */
-	List<Token> tokenStream() {
-		while (!end()) {
+	public List<Token> tokenStream() {
+		while (!checkEnd()) {
 			start = curr;
 			scanToken();
 		}
@@ -44,9 +45,9 @@ public class Lexer {
 		tokens.add(new Token(TokenType.EOF, "", null, line));
 		return tokens;
 	}
-	
+
 	/*
-	 * method that scans source code for tokens 
+	 * method that scans source code for tokens
 	 */
 	private void scanToken() {
 		char c = next();
@@ -95,14 +96,14 @@ public class Lexer {
 			break;
 		case '/':
 			if (match('/')) {
-				while (getCurrChar()!='\n' && !end()) {
+				while (getCurrChar() != '\n' && !checkEnd()) {
 					next();
 				}
-				
+
 			} else {
 				tokenize(TokenType.SLASH);
 			}
-		
+
 		case ' ':
 		case '\r':
 		case '\t':
@@ -118,13 +119,13 @@ public class Lexer {
 			if (isDigit(c)) {
 				tokenizeNumber();
 			} else if (isAlpha(c)) {
-				ID();
+				tokenizeID();
 			}
 			break;
 		}
 	}
-	
-	private void ID() {
+
+	private void tokenizeID() {
 		while (isAlpha(getCurrChar())) {
 			next();
 		}
@@ -133,18 +134,116 @@ public class Lexer {
 			tokenize(TokenType.ID);
 		} else {
 			tokenize(resKeywords.get(kw));
-			
+
 		}
 	}
-	
+
 	/**
-	 * method that iterates to the next char in the source code
-	 * and returns the previous char
+	 * method that iterates to the next char in the source code and returns the
+	 * previous char
+	 * 
 	 * @return char
 	 */
 	private char next() {
+
 		curr++;
-		return source.charAt(curr -1);
+
+		return source.charAt(curr - 1);
+
+	}
+
+	
+
+	/*
+	 * method that checks for second char in 2 character tokens. Specifically, is
+	 * looking for = in !=, <=, etc.
+	 * 
+	 */
+	private boolean match(char input) {
+//		if (checkEnd()) {
+//			return false;
+//		}
+//		else if (source.charAt(curr)!= input) {
+//			return false;
+//		}
+//		curr++;
+//		return true;
+		if (checkEnd())
+			return false;
+		if (source.charAt(curr) != input)
+			return false;
+
+		curr++;
+		return true;
+	}
+
+	/*
+	 * method that returns current char
+	 */
+	private char getCurrChar() {
+		if (checkEnd()) {
+			return '\0';
+		}
+		return source.charAt(curr);
+	}
+
+	private char getNextChar() {
+		if (curr + 1 >= source.length()) {
+			return '\0';
+		}
+		return source.charAt(curr + 1);
+	}
+	
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+	}
+
+	/*
+	 * checks if char is a digit
+	 */
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+
+	/*
+	 * method for tokenizing numbers
+	 */
+	private void tokenizeNumber() {
+		while (isDigit(getCurrChar())) {
+			next();
+		}
+
+		// if . is encountered check to see if next char is a number
+		if (getCurrChar() == '.' && isDigit(getNextChar())) {
+			next();
+			while (isDigit(getCurrChar())) {
+				next();
+			}
+			tokenize(TokenType.NUMBER, Double.parseDouble(source.substring(start, curr)));
+		}
+
+		tokenize(TokenType.NUMBER, Integer.parseInt(source.substring(start, curr)));
+				
+
+	}
+
+	/*
+	 * method for tokenizing strings
+	 */
+	private void tokenizeString() {
+		while (getCurrChar() != '"' && !checkEnd()) {
+			if (getCurrChar() == '\n') {
+				line++;
+				
+			}
+			next();
+		}
+	
+		next();
+
+		String string = source.substring(start + 1, curr - 1);
+		tokenize(TokenType.STRING, string);
+
 	}
 	
 	/*
@@ -153,6 +252,7 @@ public class Lexer {
 	private void tokenize(TokenType tokenType) {
 		tokenize(tokenType, null);
 	}
+
 	/*
 	 * method that makes tokens for literals with values
 	 */
@@ -160,95 +260,12 @@ public class Lexer {
 		String text = source.substring(start, curr);
 		tokens.add(new Token(tokenType, text, literal, line));
 	}
-	
-	/*
-	 * method that checks for second char in 2 character tokens.
-	 * Specifically, is looking for = in !=, <=, etc.
-	 * 
-	 */
-	private boolean match(char input) {
-		if (end()) {
-			return false;
-		}
-		else if (source.charAt(curr)!= input) {
-			return false;
-		}
-		curr++;
-		return true;
-	}
-	
-	/*
-	 * method that returns current char
-	 */
-	private char getCurrChar() {              
-	    return source.charAt(curr);
-	  }     
-	private char getNextChar() {
-		return source.charAt(curr+1);
-	}
-	
-	
-	
-	private boolean isAlpha(char c) {
-		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
-	}
-	
-	
-	
-	/*
-	 * checks if char is a digit
-	 */
-	private boolean isDigit(char c) {
-		return c >= '0' && c <= '9';
-	}
-	
-	/*
-	 * method for tokenizing numbers
-	 */
-	private void tokenizeNumber() {
-		while (isDigit(getCurrChar())) {
-			next();
-		}
-		
-		//if . is encountered check to see if next char is a number
-		if (getCurrChar() =='.' && isDigit(getNextChar())) {
-			next();
-		}
-		
-		while (isDigit(getCurrChar())) {
-			next();
-		}
-		
-		tokenize(TokenType.NUMBER, Double.parseDouble(source.substring(start,curr)));
-		
-		
-	}
-	
-	/*
-	 * method for tokenizing strings
-	 */
-	private void tokenizeString() {
-		while (getCurrChar() != '\n' && !end()) {
-			if(getCurrChar() == '\n') {
-				line++;
-			}
-			next();
-		}
-		
-		next();
-		
-		String value = source.substring(start +1, curr-1 );
-		tokenize(TokenType.STRING, value);
-		
-	}
-	
 	/*
 	 * method that checks if all chars have been iterated through.
 	 */
-	private boolean end() {
-		boolean isEnd = curr > source.length();
+	private boolean checkEnd() {
+		isEnd = curr >= source.length();
 		return isEnd;
 	}
-	
 
 }
